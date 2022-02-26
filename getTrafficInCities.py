7d2457df5dd6d67733dba2890af1929ad992865f
zhaoluyun <zhaoluyun1992.student@sina.com>
2022-02-26 08:55:03 +0800

    Update getTrafficInCities.py

--- getTrafficInCities.py
+++ getTrafficInCities.py
@@ -9,13 +9,13 @@ import single_getlnglat
 thread_list = [] 
 
 
-for i in range(0,37):
-    inputfilename = 'input/input_'+str(i)
-    outputfilename_lipetsk = 'output/output_lipetsk_'+str(i)
-    outputfilename_perm = 'output/output_perm_'+str(i)
-    out_error_name = 'output/error_'+str(i)
+for i in range(0,10):
+    inputfilename = 'input_omsk/input_omsk_'+str(i)
+    outputfilename = 'output_omsk/output_omsk_'+str(i)
+    out_other_name = 'output_omsk/omsk_other_'+str(i)
+    out_error_name = 'output_omsk/error_'+str(i)
 
-    t=threading.Thread(target=single_getlnglat.track,args=(inputfilename,outputfilename_lipetsk,outputfilename_perm,out_error_name,)) # 创建子线程
+    t=threading.Thread(target=single_getlnglat.track,args=(inputfilename,outputfilename,out_other_name,out_error_name,)) 
     thread_list.append(t)
 
 
